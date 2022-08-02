c7acf82089058aff6a0f09849d4f18bea117263e
Fret <clemfret7@gmail.com>
2022-08-02 16:04:38 +0200

    Add tests

---
+++ operations.js
@@ -0,0 +1,17 @@
+export default {
+  divide(numberOne, numberTwo) {
+    return numberOne / numberTwo;
+  },
+  
+  multiply(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+  },
+  
+  subtract(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+  },
+  
+  add(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+  }
+}
---
+++ operations.test.js
@@ -0,0 +1,17 @@
+const Operation = require('./operations.js');
+
+test('divide 10 by 2 to equal 5', () => {
+  expect(Operation.divide(10, 2)).toBe(5);
+});
+
+test('multiply 10 by 2 to equal 20', () => {
+  expect(Operation.multiply(10, 2)).toBe(20);
+});
+
+test('subtract 10 by 2 to equal 8', () => {
+  expect(Operation.subtract(10, 2)).toBe(8);
+});
+
+test('add 10 by 2 to equal 12', () => {
+  expect(Operation.add(10, 2)).toBe(12);
+});
